6b2406cfd90248ccae3b77769f8f61c68d06b10c
Tate Bosler <tatebosler@gmail.com>
2016-03-17 23:55:36 -0500

    Added some methods for show publication

--- server/publish.js
+++ server/publish.js
@@ -1,5 +1,14 @@
 Meteor.publish("users", function() {
 	return Meteor.users.find({}, {fields: {
 		"krlx": 1
-	}})
+	}});
+});
+
+Meteor.publish("myShows", function() {
+	if(this.userId) {
+		var userNetID = Meteor.users.findOne({"_id": this.userId});
+		return Shows.find({$or: [{owner: userNetID}, {djs: userNetID}]});
+	} else {
+		return this.ready();
+	}
 })
