bd4938b9e42c062c9741ecb6f8137e8e610e01b1
Tate Bosler <tatebosler@gmail.com>
2016-03-19 14:03:37 -0500

    Activated Add DJ button, for #2
    The button only works if the last form field is not empty (or full of
    whitespace).

--- client/templates/application/1-djs/1-djs.js
+++ client/templates/application/1-djs/1-djs.js
@@ -1,5 +1,29 @@
 Template.step1.helpers({
+	djList: function() {
+		return Session.get("djs");
+	},
 	displayIndex: function(index) {
 		return index + 1;
+	}
+})
+
+Template.step1.created = function() {
+	Session.set("djs", this.data.djs);
+}
+
+Template.step1.events({
+	"click #addDJ": function() {
+		var djs = Session.get("djs");
+		if(djs[djs.length - 1].trim().length > 0) {
+			djs.push("");
+			Session.set("djs", djs);
+		}
+	},
+	"change .dj-input": function(event) {
+		var index = parseInt(event.currentTarget.dataset.index);
+		var newValue = event.currentTarget.value;
+		var djs = Session.get("djs");
+		djs[index] = newValue;
+		Session.set("djs", djs);
 	}
 })
