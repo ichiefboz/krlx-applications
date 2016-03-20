043245bd87d77376406dd4eac06d5d155aa801f9
Tate Bosler <tatebosler@gmail.com>
2016-03-20 01:51:12 -0500

    Added forms for DJ contact details

--- client/templates/application/2-contact/2-contact.js
+++ client/templates/application/2-contact/2-contact.js
@@ -7,3 +7,24 @@ Template.step2.helpers({
 Template.step2.rendered = function() {
 	Meteor.subscribe("djsInShow", this.data._id);
 }
+
+Template.contactForm.helpers({
+	displayName: function() {
+		return (this.name == null) ? this.netid : this.name;
+	},
+	selected: function(value) {
+		return (this.year == value) ? "selected" : "";
+	},
+	years: function() {
+		var year = (new Date).getFullYear();
+		var years = [];
+		for(var i = 0; i < 5; i++) {
+			years.push(year + i);
+		}
+		return years;
+	}
+})
+
+Template.contactForm.rendered = function() {
+	$("select.dropdown").dropdown();
+}
