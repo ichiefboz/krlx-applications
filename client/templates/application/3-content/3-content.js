a7c9fea0e83e6f82b9bfd5c16814be9ba2cadf75
Tate Bosler <tatebosler@gmail.com>
2016-03-20 23:56:41 -0500

    Added all form fields and validation to content screen, relevant to #5

--- client/templates/application/3-content/3-content.js
+++ client/templates/application/3-content/3-content.js
@@ -6,3 +6,47 @@ Template.step3.helpers({
 		return this.type == "Student Org";
 	}
 })
+
+Template.step3.rendered = function() {
+	var fieldRules = {};
+	switch(this.data.type) {
+		case "Student Org":
+			fieldRules["sponsor"] = {rules: [
+				{type: "empty", prompt: "You must specify the organization sponsoring this show"},
+				{type: "minLength[3]", prompt: "Please specify the full name of your sponsoring organization, or an abbreviation if that is how you are commonly referred to on campus (e.g. CANOE, SAPB, CSA, or KRLX, but not Tonian)"}
+			]};
+		case "Standard":
+			fieldRules["title"] = {rules: [
+				{type: "empty", prompt: "Please choose a show name"},
+				{type: "minLength[3]", prompt: "Please choose a show name that is at least 3 characters long"},
+				{type: "doesntContain[shit]", prompt: "KRLX guidelines prohibit the word shit in show titles"},
+				{type: "doesntContain[piss]", prompt: "KRLX guidelines prohibit the word piss in show titles"},
+				{type: "doesntContain[fuck]", prompt: "KRLX guidelines prohibit the word fuck in show titles"},
+				{type: "doesntContain[cunt]", prompt: "KRLX guidelines prohibit the word cunt in show titles"},
+				{type: "doesntContain[cocksucker]", prompt: "KRLX guidelines prohibit the word cocksucker in show titles"},
+				{type: "doesntContain[tits]", prompt: "KRLX guidelines prohibit the word tits in show titles"}
+			]};
+			break;
+		case "Bandemonium":
+			fieldRules["theme"] = {rules: [
+				{type: "empty", prompt: "You need to specify the artist or theme that you'll be exploring during your show"},
+			]};
+			fieldRules["why"] = {rules: [
+				{type: "minLength[100]", prompt: "Your response to why we should pick your show must be 100 characters or longer"}
+			]};
+			fieldRules["description"] = {rules: [
+				{type: "minLength[30]", prompt: "The description of the show, which will be posted to the web, must be at least 30 characters, however we encourage you to describe your show in as much detail as you wish"}
+			]};
+			break;
+	}
+	$(".ui.form").form({fields: fieldRules});
+}
+
+Template.step3.events({
+	"submit .ui.form": function(event) {
+		event.preventDefault();
+		if($(".ui.form").form("is valid")) {
+			alert("Validation successful");
+		}
+	}
+})
