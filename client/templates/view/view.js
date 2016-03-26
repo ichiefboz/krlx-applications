06e8d69cdb2f0596b079a87917d7948dbdaaeb77
Tate Bosler <tatebosler@gmail.com>
2016-03-26 18:16:30 -0500

    Added remaining Standard/Org data

--- client/templates/view/view.js
+++ client/templates/view/view.js
@@ -26,5 +26,41 @@ Template.viewShow.helpers({
 	color: function(prio) {
 		var colors = {A: "black", B: "grey", C: "violet", D: "blue", E: "teal", F: "green", G: "olive", H: "yellow", I: "orange", J: "red"};
 		return colors[prio];
+	},
+	displayName: function(netid) {
+		var dj = DJs.findOne({netid: netid});
+		return (dj.name == null) ? netid : dj.name;
+	},
+	classes: function() {
+		return this.conflicts.classes.join(", ");
+	},
+	conflicts: function() {
+		return this.badSundays.join(", ");
+	},
+	displayDays: function(days) {
+		var weekdays = {
+			sun: {display: "Sunday", color: "grey"},
+			mon: {display: "Monday", color: "olive"},
+			tue: {display: "Tuesday", color: "red"},
+			wed: {display: "Wednesday", color: "blue"},
+			thu: {display: "Thursday", color: "green"},
+			fri: {display: "Friday", color: "yellow"},
+			sat: {display: "Saturday", color: "brown"}
+		};
+		var returnDays = [];
+		for(var i = 0; i < days.length; i++) {
+			returnDays.push(weekdays[days[i]]);
+		}
+		return returnDays;
+	},
+	displayTime: function(time) {
+		// time comes in as 24 hours
+		var timeSplit = time.split(":");
+		var hours = timeSplit[0];
+		var am = (hours >= 12 && hours <= 23) ? "PM" : "AM";
+		var displayHours = (hours >= 12) ? hours - 12 : hours;
+		if(displayHours == 0) displayHours = 12;
+
+		return displayHours + ":" + timeSplit[1] + " " + am;
 	}
 })
